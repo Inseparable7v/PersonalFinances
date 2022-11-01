10872bb667137a5d5f63c37dfee72869707fa720
D-Todorov <daniel.todorow1@gmail.com>
2022-11-01 23:08:52 +0200

    Add project files.

---
+++ PersonalFinances/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinances
+{
+    public class User
+    {
+        public User()
+        {
+             
+        }
+
+        public int UserId { get; set; }
+
+        public string FirstName { get; set; }
+        public string SurrName { get; set; }
+        public string LastName { get; set; }
+
+        public int IdentificationNumber { get; set; }
+
+        public int Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
